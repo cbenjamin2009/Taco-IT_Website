a8d15f0f2d21c963fb2518dac39acc96a4faed8e
Chris <cbenjamin2009@gmail.com>
2021-10-19 17:30:32 -0700

    remove unused state

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'
 
 function Home() {
-    const [homePage, sethomePage] = useState(true)
     
     return (
         <div className="homePage">
             <h1 className="titleText">Let's Taco Bout Your IT Needs Today!</h1>
-            
+
         </div>
     )
 }
