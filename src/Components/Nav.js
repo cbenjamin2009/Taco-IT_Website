4f2b1c610dd1747e176fdccfe96210e053459657
Chris <cbenjamin2009@gmail.com>
2021-12-13 21:17:54 -0800

    progress on removing work section

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,66 +1,11 @@
-import React, { useState } from 'react'
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
-import Home from './Home'
-import About from './About'
-import Work from './Work'
-import Contact from './Contact'
-import Services from './Services'
-import ErrorPage from './ErrorPage'
+import React from 'react'
 import HomeIcon from "@mui/icons-material/Home"
 import ContactMailIcon from '@mui/icons-material/ContactMail';
 import InfoIcon from '@mui/icons-material/Info';
 import DesignServicesIcon from '@mui/icons-material/DesignServices';
-import BusinessIcon from '@mui/icons-material/Business';
 
 function Nav() {
-    // const [active, setActive] = useState('Home')
-
-    // let nav;
-    // let icon;
-
-    // //cleanup all active class states 
-    // function cleanup(){
-    //     let allnav = document.querySelectorAll('.nav-link')
-    //     allnav.forEach(e => e.classList.remove('active'))
-    // }
-
-        //set active class based on link clicked. 
-    // switch(active){
-    //     case 'Contact': 
-    //         cleanup();
-    //         nav = document.getElementById('nav-link-contact')
-    //         nav.classList.add('active');
-    //         icon = document.getElementById('ContactIcon')
-    //         icon.classList.add('active')
-    //         break;
-    //     case 'About':
-    //         cleanup();
-    //         nav = document.getElementById('nav-link-about')
-    //         nav.classList.add('active')
-    //         icon = document.getElementById('AboutIcon')
-    //         icon.classList.add('active')
-    //         break;
-    //     case 'Services':
-    //         cleanup();
-    //         nav = document.getElementById('nav-link-services')
-    //         nav.classList.add('active')
-    //         icon = document.getElementById('ServicesIcon')
-    //         icon.classList.add('active')
-    //         break;
-    //     case 'Work':
-    //         cleanup();
-    //         nav = document.getElementById('nav-link-work')
-    //         nav.classList.add('active')
-    //         icon = document.getElementById("WorkIcon")
-    //         icon.classList.add('active')
-    //         break;
-    //     default:
-    //         cleanup();
-    //         nav = document.getElementById('nav-link-home')
-    //         // nav.classList.add('active')
-    //         break;
-    // }
-    
+      
     return (
         <nav className="">
             <ul className="w-full flex flex-grow lg:flex lg:items-center lg:w-auto space-x-4 text-white mr-10">
@@ -79,10 +24,6 @@ function Nav() {
                 <li className="flex flex-col items-center">
                 <DesignServicesIcon id="ServicesIcon"  className=""/>
                     <a href='#services' id="nav-link-services"  className="">Services</a>
-                </li>
-                <li className="flex flex-col items-center">
-                <BusinessIcon id="WorkIcon" className=""/>
-                    <a href='#work' id="nav-link-work"  className="">Work </a>
                 </li>
 
             </ul>
