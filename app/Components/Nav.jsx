4a30dc7cfe9eec1451fb81be2a04f899e7f73a97
Chris <cbenjamin2009@gmail.com>
2021-12-24 11:14:10 -0800

    update nav spacing

--- app/Components/Nav.jsx
+++ app/Components/Nav.jsx
@@ -8,7 +8,7 @@ function Nav() {
       
     return (
         <nav className="">
-            <ul className="w-full flex lg:flex lg:items-center lg:w-auto space-x-10 text-2xl text-white mr-10 sm:mt-10">
+            <ul className="w-full flex lg:flex lg:items-center lg:w-auto space-x-4 md:space-x-10 text-2xl text-white mr-10 sm:mt-10">
                 <li className="flex flex-col items-center"> 
                     <HomeIcon/>
                     <a href='/' id="nav-link-home">Home</a>
