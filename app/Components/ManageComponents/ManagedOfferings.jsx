c837c7948beff239f475dcef86f7b4e9fe5ecfa3
Chris <cbenjamin2009@gmail.com>
2022-01-02 14:04:21 -0800

    manged offering changes

--- app/Components/ManageComponents/ManagedOfferings.jsx
+++ app/Components/ManageComponents/ManagedOfferings.jsx
@@ -8,42 +8,39 @@ import PersonPinIcon from '@mui/icons-material/PersonPin';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 function ManagedOfferings() {
     return (
-        <section className="ManagedOfferings pt-6">
-            <p className="text-center text-3xl text-white pt-6">Managed IT Services</p>
-
-        <div className="columns-1 md:columns-2 w-1/2">
-            <div className="flex">
-            <MonitorHeartIcon />
+        <div className="manageSection">
+        
+            <div className=" p-4 border-2">
+            <MonitorHeartIcon fontSize="large" color="success"/>
             <Offerings title={'Monitoring'} info={'Proactive Server and Workstation Monitoring'}/>
             </div>
-            <div className="flex">
-            <CoronavirusIcon/>
+            <div className="   p-4">
+            <CoronavirusIcon fontSize="large"  color="success"/>
             <Offerings title={'Managed Anti-Virus'} info={'Enterprise Class Anti-Virus Protection for all of your devices'}/>
             </div>
-            <div className="flex">
-            <StorageIcon />
+            <div className="   p-4">
+            <StorageIcon  fontSize="large"  color="success"/>
             <Offerings title={'Managed Backup'} info={'Managed local and cloud backup with quarterly testing reports'}/>
             </div>
-            <div className="flex">
-            <LocalPostOfficeIcon />
+            <div className="   p-4">
+            <LocalPostOfficeIcon  fontSize="large"  color="success"/>
             <Offerings title={'Managed Microsoft 365'} info={'Let us manage your Microsoft 365 Business Plan'}/>
             </div>
-            <div className="flex">
-                <KeyboardIcon/>
+            <div className="  p-4">
+                <KeyboardIcon fontSize="large"  color="success"/>
             <Offerings title={'Remote Support'} info={'Fast and Secure Remote support assistance allows for faster response times and less staff interruption'}/>
             </div>
-            <div className="flex">
-                <PersonPinIcon/>
+            <div className="  p-4">
+                <PersonPinIcon fontSize="large"  color="success"/>
             <Offerings title={'Onsite Support'} info={'When we cannot fix it remotely, we come to you and fix it on the spot'}/>
             </div>
-            <div className="flex">
-                <AlternateEmailIcon />
+            <div className="  p-4">
+                <AlternateEmailIcon  fontSize="large"  color="success"/>
             <Offerings title={'Managed Email and Spam Services'} info={'No one likes spam, let us help you reduce your spam and malicious emails'}/>
             </div>
 
-        </div>
         
-        </section>
+        </div>
     )
 }
 
